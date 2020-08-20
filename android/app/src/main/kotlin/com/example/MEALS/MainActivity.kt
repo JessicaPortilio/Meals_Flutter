3e5c80d92825b1008e839d1cc1504cadfd6e42bb
JessicaPortilio <jessicaket91@gmail.com>
2020-08-20 14:28:20 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/MEALS/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.MEALS
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,70 @@
+import 'package:MEALS/components/main_drawer.dart';
+import 'package:MEALS/models/meal.dart';
+import 'package:MEALS/screens/categories_screens.dart';
+import 'package:MEALS/screens/favorite_screen.dart';
+import 'package:flutter/material.dart';
+
+class TabsScreen extends StatefulWidget {
+  final List<Meal> favoriteMeals;
+  const TabsScreen(this.favoriteMeals);
+
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedScreenIndex = 0;
+  List<Map<String, Object>> _screens;
+
+  @override
+  void initState() {
+    super.initState();
+    _screens = [
+      {
+        'title': 'Lista de Categorias',
+        'screen': CategoriesScreens(),
+      },
+      {
+        'title': 'Meus Favoritos',
+        'screen': FavoriteScreen(widget.favoriteMeals),
+      },
+    ];
+  }
+
+  _selectScreen(int index) {
+    setState(() {
+      _selectedScreenIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          _screens[_selectedScreenIndex]['title'],
+        ),
+      ),
+      drawer: MainDrawer(),
+      body: _screens[_selectedScreenIndex]['screen'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectScreen,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedScreenIndex,
+        //type: BottomNavigationBarType.shifting,  Estilo pra fazer animação
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            title: Text('Categorias'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            title: Text('Favoritos'),
+          ),
+        ],
+      ),
+    );
+  }
+}
